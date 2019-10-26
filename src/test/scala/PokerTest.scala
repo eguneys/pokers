ccb500836e3a458c6138208b84278a17fcc5d191
Emre Guneyler <eguneys@hotmail.com>
2019-10-26 22:49:13 +0000

    update add test

---
+++ src/test/scala/PokerTest.scala
@@ -0,0 +1,8 @@
+import org.specs2.matcher.{ /* Matcher, */ ValidationMatchers }
+import org.specs2.mutable.Specification
+
+trait PokerTest extends Specification with ValidationMatchers {
+
+  
+
+}
